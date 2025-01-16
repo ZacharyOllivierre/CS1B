e3cc1a083822cfba054fcc5af044aa61c240d0ac
Zachary Ollivierre <zollivierre0@saddleback.edu>
2025-01-16 04:48:29 +0000

    Jan 15

--- Prob1.cpp
+++ Prob1.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
 
+int main() {
+    string s = "aaaabbbbcccc";
+    string output = "";
+    int i = 0;
+
+    while (i < s.length()) {
+        char ch = s[i++];
+        int counter = 1;
+        
+        while (i < s.length() and s[i] == ch) {
+            counter++;
+            i++;
+        }
+        output += ch;
+        output += to_string(counter);
+    }
+    cout << output << endl;
+    return 0;
+}
